a8a00a5c18474c1b0fa59d1e44125d007e29a252
Sindre Sorhus <sindresorhus@gmail.com>
2018-10-24 07:49:14 -0300

    Update index.test-d.ts
    Co-Authored-By: Fazendaaa <lucas.carotta@outlook.com>

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,5 +1,5 @@
 import {expectType} from 'tsd-check';
-import shellEnv, { sync, ShellEnv } from '.';
+import shellEnv, {sync, ShellEnv} from '.';
 
 (async () => {
     expectType<ShellEnv>(sync());
