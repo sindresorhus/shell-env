d12d44da8a5c388427d12987b47cdbb0418d9329
Dimitri Benin <dimitri.benin@gmail.com>
2019-04-06 02:23:15 +0200

    Refactor TypeScript definition to CommonJS compatible export

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,36 @@
-export interface EnvironmentVariables {
-	readonly [key: string]: string;
+declare namespace shellEnv {
+	interface EnvironmentVariables {
+		readonly [key: string]: string;
+	}
 }
 
-/**
- * Get the environment variables defined in your dotfiles.
- *
- * @param shell - The shell to read environment variables from. Default: User default shell.
- * @returns The environment variables.
- */
-export function sync(shell?: string): EnvironmentVariables;
+declare const shellEnv: {
+	/**
+	Get the environment variables defined in your dotfiles.
 
-/**
- * Get the environment variables defined in your dotfiles.
- *
- * @param shell - The shell to read environment variables from. Default: User default shell.
- * @returns The environment variables.
- */
-export default function shellEnv(shell?: string): Promise<EnvironmentVariables>;
+	@param shell - The shell to read environment variables from. Default: User default shell.
+	@returns The environment variables.
+
+	@example
+	```
+	import shellEnv = require('shell-env');
+
+	console.log(shellEnv.sync());
+	//=> {TERM_PROGRAM: 'Apple_Terminal', SHELL: '/bin/zsh', ...}
+
+	console.log(shellEnv.sync('/bin/bash'));
+	//=> {TERM_PROGRAM: 'iTerm.app', SHELL: '/bin/zsh', ...}
+	```
+	*/
+	(shell?: string): Promise<shellEnv.EnvironmentVariables>;
+
+	/**
+	Get the environment variables defined in your dotfiles.
+
+	@param shell - The shell to read environment variables from. Default: User default shell.
+	@returns The environment variables.
+	*/
+	sync(shell?: string): shellEnv.EnvironmentVariables;
+};
+
+export = shellEnv;
